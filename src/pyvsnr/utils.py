e02f279f7e962ccbecf09fdfda8a7aa9359eb50a
patquem <patrick.quemere@cea.fr>
2021-12-15 10:38:41 +0100

    revisit shape management when shape_ref is smallest than initial shape

--- src/pyvsnr/utils.py
+++ src/pyvsnr/utils.py
@@ -8,9 +8,6 @@ def pad_centered(arr, shape_ref, value=0):
     """ Pad array with value in a centered way """
     assert (len(shape_ref) == len(arr.shape))
 
-    # to deal with shape_ref smaller than initial shape
-    shape_ref = tuple(map(max, arr.shape, shape_ref))
-
     dim = len(shape_ref)
     arr_pad = arr.copy()
     for k in range(dim):
@@ -21,10 +18,15 @@ def pad_centered(arr, shape_ref, value=0):
         # swap axes to work on axis=0
         arr_pad = np.swapaxes(arr_pad, 0, k)
 
-        width = (gap2, gap - gap2)
-        if dim > 1:
-            width = (width,) + (dim - 1) * ((0, 0),)
-        arr_pad = np.pad(arr_pad, width, constant_values=value)
+        # padding
+        if gap >= 0:
+            width = (gap2, gap - gap2)
+            if dim > 1:
+                width = (width,) + (dim - 1) * ((0, 0),)
+            arr_pad = np.pad(arr_pad, width, constant_values=value)
+        # cutting
+        else:
+            arr_pad = arr_pad[-gap2:-gap2 + shape_ref[k], ...]
 
         # return to original axis
         arr_pad = np.swapaxes(arr_pad, 0, k)
