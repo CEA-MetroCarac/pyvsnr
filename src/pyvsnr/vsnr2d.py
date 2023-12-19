fixed normalization

src/pyvsnr/vsnr2d.py
@@ -1,5 +1,21 @@
+"""
+This module contains the 2D-VSNR algorithm implementation.
+its a port of the original CUDA implementation to python using cupy.
+It contains several improvements in terms of performance and memory usage.
+"""
 import numpy as np
 
+def vmax_encoding(arr):
+    """ Basic function to estimate vmax value related to array type encoding"""
+    if arr.max() <= 1.:
+        return 1.
+    elif arr.max() <= 255:
+        return 255
+    elif arr.max() <= 65535:
+        return 65535
+    else:
+        raise IOError("maximum value over 65535 !!!")
+    
 def compute_phi(fphi1, fphi2, beta, xp):
     """
     Compute the value of fphi based on the values of fphi1, fphi2, and beta.
@@ -10,28 +26,31 @@ def compute_phi(fphi1, fphi2, beta, xp):
 
     # Compute the value of fphi
     fphi = 1 + beta * (fphi1_squared_real + fphi2_squared_real)
-    
+
     # Create a complex CuPy array with the computed values of fphi
     fphi_complex = xp.zeros_like(fphi, dtype=xp.complex64)
     fphi_complex.real = fphi
 
     return fphi_complex
 
-def update_psi(fpsitemp, fsum,alpha, xp):
+
+def update_psi(fpsitemp, fsum, alpha, xp):
     """
     Update the value of fsum based on the values of fpsitemp, fsum, and alpha.
     """
-    #// fsum += |fpsitemp|^2 / alpha_i;
-    fsum=xp.add(fsum,xp.divide(fpsitemp,alpha))
+    # // fsum += |fpsitemp|^2 / alpha_i;
+    fsum = xp.add(fsum, xp.divide(fpsitemp, alpha))
     return fsum
+
 
 def create_dirac(n, val, xp):
     """
     Create a Dirac filter.
     """
-    psi=xp.zeros((n,), dtype=xp.float32)
-    psi[0]=val
+    psi = xp.zeros((n,), dtype=xp.float32)
+    psi[0] = val
     return psi
+
 
 def create_gabor(n0, n1, level, sigmax, sigmay, angle, phase, lambda_, xp):
     """
@@ -54,210 +73,232 @@ def create_gabor(n0, n1, level, sigmax, sigmay, angle, phase, lambda_, xp):
     x_theta = x * xp.cos(theta) + y * xp.sin(theta)
     y_theta = y * xp.cos(theta) - x * xp.sin(theta)
 
-    val = xp.exp(-0.5 * ((x_theta / sigmax) ** 2 + (y_theta / sigmay) ** 2)) * xp.cos((x_theta * lambda_ / sigmax) + phase)
+    val = xp.exp(
+        -0.5 * ((x_theta / sigmax) ** 2 + (y_theta / sigmay) ** 2)
+    ) * xp.cos((x_theta * lambda_ / sigmax) + phase)
     psi = level * val / nn
 
     return psi.ravel()
+
 
 def update_y(d1u0, d2u0, tmp1, tmp2, lambda1, lambda2, beta, xp):
     """
     Update the values of y1 and y2 based on the values of d1u0, d2u0, tmp1,
     tmp2, lambda1, lambda2, and beta.
     """
-    t1=d1u0-(tmp1+(lambda1/beta))
-    t2=d2u0-(tmp2+(lambda2/beta))
-    ng=xp.sqrt(xp.add(xp.square(t1),xp.square(t2))) + xp.finfo(float).eps # Adding a small epsilon to avoid division by zero
+    t1 = d1u0 - (tmp1 + (lambda1 / beta))
+    t2 = d2u0 - (tmp2 + (lambda2 / beta))
+    ng = (
+        xp.sqrt(xp.add(xp.square(t1), xp.square(t2))) + xp.finfo(float).eps
+    )  # Adding a small epsilon to avoid division by zero
     mask = ng > 1.0 / beta
 
     coef = 1.0 - (1.0 / (beta * ng))
-    y1 = d1u0 - mask * (t1 * coef) # cp inf * 0 = 0
-    y2 = d2u0 - mask * (t2 * coef) # while np inf * 0 = nan
+    y1 = d1u0 - mask * (t1 * coef)  # cp inf * 0 = 0
+    y2 = d2u0 - mask * (t2 * coef)  # while np inf * 0 = nan
 
     return y1, y2
 
+
 def setd1(n, n1, xp):
-    d1=xp.zeros((n,), dtype=xp.complex64)
-    d1[0]=1
-    d1[n1-1]=-1
+    """
+    Set the values of d1.
+    """
+    d1 = xp.zeros((n,), dtype=xp.complex64)
+    d1[0] = 1
+    d1[n1 - 1] = -1
 
     return d1
 
+
 def setd2(n, n1, xp):
-    d2=xp.zeros((n,), dtype=xp.complex64)
-    d2[0]=1
-    d2[n-n1]=-1
-    
+    """
+    Set the values of d2.
+    """
+    d2 = xp.zeros((n,), dtype=xp.complex64)
+    d2[0] = 1
+    d2[n - n1] = -1
+
     return d2
 
-def VSNR_2D_FIJI_GPU(filters, u0, n0, n1, nit, beta, max, xp):
+
+def compute_vsnr(filters, u0, n0, n1, nit, beta, vmax, xp):
     """
     Calculate the corrected image using the 2D-VSNR algorithm
     """
-    n=n0*n1
-    gu=xp.zeros((n,), dtype=xp.float32)
+    n = n0 * n1
+    gu = xp.zeros((n,), dtype=xp.float32)
 
-    gu0=u0.copy()
-    gpsi=xp.zeros((n,), dtype=xp.float32)
+    gu0 = u0.copy()
+    gpsi = xp.zeros((n,), dtype=xp.float32)
 
-    gu0=xp.divide(gu0,max)
+    gu0 = xp.divide(gu0, vmax)
 
     # 2. Prepares filters
-    gpsi=create_filters(filters, gu0, n0, n1, xp)
+    gpsi = create_filters(filters, gu0, n0, n1, xp)
 
     # 3. Denoises the image
-    gu=vsnr_admm(gu0, gpsi, n0, n1, nit, beta, xp)
-    
+    gu = vsnr_admm(gu0, gpsi, n0, n1, nit, beta, xp)
+
     # 4. Copies the result to u
-    gu=xp.multiply(gu,max)
-    u=xp.copy(gu)
+    gu = xp.multiply(gu, vmax)
+    u = xp.copy(gu)
 
     return u
 
+
 def create_filters(filters, gu0, n0, n1, xp):
     """
-    Create filters based on filters parameters stored in the filters array. The calculated filters are stored in the gpsi array.
+    Create filters based on filters parameters stored in the filters array.
+    The calculated filters are stored in the gpsi array.
     """
-    i=0
     # Computes the l2 norm of u0 on GPU
-    fsum=0
-    norm=xp.linalg.norm(gu0)
+    fsum = 0
+    norm = xp.linalg.norm(gu0)
 
     # Computes d1 and fd1
-    n=n0*n1
+    n = n0 * n1
     # d1=xp.array([-3-10j,-2+2j],dtype=xp.complex64) #test
-    d1 = setd1(n, n1, xp) # // d1[0] = 1; d1[n1-1] = -1;
-    fd1=xp.fft.fft2(d1.reshape(n0,n1)).flatten() #TODO is it necessary to speicfy the plan ?? cufftPlan2d
-    fd1=xp.abs(fd1) # // fd1 = |fd1|;
+    d1 = setd1(n, n1, xp)  # // d1[0] = 1; d1[n1-1] = -1;
+    fd1 = xp.fft.fft2(d1.reshape(n0, n1)).flatten()
+    fd1 = xp.abs(fd1)  # // fd1 = |fd1|;
 
     # Computes d2 and fd2
     # d2=xp.array([5-1j,-4+2j],dtype=xp.complex64) #test
-    d2 = setd2(n, n1, xp) # // d2[0] = 1; d2[(n0-1)*n1] = -1;
-    fd2=xp.fft.fft2(d2.reshape(n0,n1)).flatten()
-    fd2=xp.abs(fd2)
+    d2 = setd2(n, n1, xp)  # // d2[0] = 1; d2[(n0-1)*n1] = -1;
+    fd2 = xp.fft.fft2(d2.reshape(n0, n1)).flatten()
+    fd2 = xp.abs(fd2)
 
     # Computes PSI = sum_{i=1}^m |PSI_i|^2/alpha_i, where alpha_i is defined in the paper.
 
     for filt in filters:
-        if filt['name'] == 'Dirac':
+        if filt["name"] == "Dirac":
             psitemp = create_dirac(n, 1, xp)
-            eta = filt['noise_level']
-        elif filt['name'] == 'Gabor':
-            sigma = filt['sigma'] 
-            theta = filt['theta']
+            eta = filt["noise_level"]
+        elif filt["name"] == "Gabor":
+            sigma = filt["sigma"]
+            theta = filt["theta"]
             psitemp = create_gabor(n0, n1, 1, sigma[0], sigma[1], theta, 0, 0, xp)
-            eta = filt['noise_level']
+            eta = filt["noise_level"]
 
-        fpsitemp = xp.fft.fft2(psitemp.reshape(n0,n1)).flatten()
+        fpsitemp = xp.fft.fft2(psitemp.reshape(n0, n1)).flatten()
 
-        fpsitemp=xp.square(xp.abs(fpsitemp)) # // fpsitemp = |fpsitemp|^2;)
-        
-        ftmp=xp.multiply(xp.abs(fd1),xp.abs(fpsitemp)) # //compute product: ftmp = |fd1|*|fpsitemp|;)
-        imax = xp.argmax(xp.abs(ftmp)) # find the index of the maximum element # cublasIsamax(handle, m, ftmp, 1, &imax);
-        max1=ftmp[imax]
+        fpsitemp = xp.square(xp.abs(fpsitemp))  # // fpsitemp = |fpsitemp|^2;)
 
-        ftmp=xp.multiply(xp.abs(fd2),xp.abs(fpsitemp))
+        ftmp = xp.multiply(xp.abs(fd1), xp.abs(fpsitemp))
+        imax = xp.argmax(xp.abs(ftmp))  # find the index of the maximum element
+        max1 = ftmp[imax]
+
+        ftmp = xp.multiply(xp.abs(fd2), xp.abs(fpsitemp))
         imax = xp.argmax(xp.abs(ftmp))
-        max2=ftmp[imax]
-        nmax=max(max1,max2)
-        alpha=np.sqrt(n)*n**2*nmax/(norm*eta)
-        
-        fsum=update_psi(fpsitemp, fsum, alpha, xp) #// fsum += |fpsitemp|^2 / alpha_i;
+        max2 = ftmp[imax]
+        nmax = max(max1, max2)
+        alpha = np.sqrt(n) * n**2 * nmax / (norm * eta)
 
-    fsum=xp.sqrt(fsum) #// fsum = sqrtf(fsum);
-    gpsi=xp.fft.ifft2(fsum.reshape(n0,n1),norm="forward").flatten()
+        fsum = update_psi(fpsitemp, fsum, alpha, xp)  # fsum += |fpsitemp|^2 / alpha_i;
+
+    fsum = xp.sqrt(fsum)  # // fsum = sqrtf(fsum);
+    gpsi = xp.fft.ifft2(fsum.reshape(n0, n1), norm="forward").flatten()
 
     return gpsi
+
 
 def vsnr_admm(u0, psi, n0, n1, nit, beta, xp):
     """
     Denoise the image u0 using the VSNR algorithm.
     """
-    n=n0*n1
+    n = n0 * n1
     # m=n0*(n1//2+1)
 
-    lambda1=xp.zeros((n,), dtype=xp.float32)
-    lambda2=xp.zeros((n,), dtype=xp.float32)
-    y1=xp.zeros((n,), dtype=xp.float32)
-    y2=xp.zeros((n,), dtype=xp.float32)
-    tmp1=xp.zeros((n,), dtype=xp.float32)
-    tmp2=xp.zeros((n,), dtype=xp.float32)
-    
-    fu0=xp.fft.fft2(u0.reshape(n0,n1)).flatten() # TODO fftn ??
-    fpsi=xp.fft.fft2(psi.reshape(n0,n1)).flatten()
+    lambda1 = xp.zeros((n,), dtype=xp.float32)
+    lambda2 = xp.zeros((n,), dtype=xp.float32)
+    y1 = xp.zeros((n,), dtype=xp.float32)
+    y2 = xp.zeros((n,), dtype=xp.float32)
+    tmp1 = xp.zeros((n,), dtype=xp.float32)
+    tmp2 = xp.zeros((n,), dtype=xp.float32)
+
+    fu0 = xp.fft.fft2(u0.reshape(n0, n1)).flatten()
+    fpsi = xp.fft.fft2(psi.reshape(n0, n1)).flatten()
 
     # // Computes d1 and fd1
-    d1 = setd1(n, n1, xp) # // d1[0] = 1; d1[n1-1] = -1;
-    fd1=xp.fft.fft2(d1.reshape(n0,n1)).flatten()
+    d1 = setd1(n, n1, xp)  # // d1[0] = 1; d1[n1-1] = -1;
+    fd1 = xp.fft.fft2(d1.reshape(n0, n1)).flatten()
 
     # // Computes d2 and fd2
-    d2 = setd2(n, n1, xp) # // d2[0] = 1; d2[(n0-1)*n1] = -1;
-    fd2=xp.fft.fft2(d2.reshape(n0,n1)).flatten()
+    d2 = setd2(n, n1, xp)  # // d2[0] = 1; d2[(n0-1)*n1] = -1;
+    fd2 = xp.fft.fft2(d2.reshape(n0, n1)).flatten()
 
     # // Computes d1u0
-    ftmp1=xp.multiply(fd1,fu0)
-    d1u0=xp.real(xp.fft.ifft2(ftmp1.reshape(n0,n1),norm="forward").flatten()) # TODO is this correct xp.real ? #should be float but is complex
-    d1u0=xp.divide(d1u0,n) # d1u0=normalize_array(d1u0)
-    
+    ftmp1 = xp.multiply(fd1, fu0)
+    d1u0 = xp.real(
+        xp.fft.ifft2(ftmp1.reshape(n0, n1), norm="forward").flatten()
+    )
+    d1u0 = xp.divide(d1u0, n)  # d1u0=normalize_array(d1u0)
+
     # // Computes d2u0
-    ftmp2=xp.multiply(fd2,fu0)
-    d2u0=xp.real(xp.fft.ifft2(ftmp2.reshape(n0,n1),norm="forward").flatten()) #TODO xp.fft.ifftn or xp.fft.ifft ??
-    d2u0=xp.divide(d2u0,n)
+    ftmp2 = xp.multiply(fd2, fu0)
+    d2u0 = xp.real(
+        xp.fft.ifft2(ftmp2.reshape(n0, n1), norm="forward").flatten()
+    )
+    d2u0 = xp.divide(d2u0, n)
 
     # // Computes fphi1 and fphi2
-    fphi1=xp.multiply(fpsi,fd1)
-    fphi2=xp.multiply(fpsi,fd2)
+    fphi1 = xp.multiply(fpsi, fd1)
+    fphi2 = xp.multiply(fpsi, fd2)
 
     # // Computes fphi
-    fphi=compute_phi(fphi1, fphi2, beta, xp)
-    
-    for k in range (nit):
+    fphi = compute_phi(fphi1, fphi2, beta, xp)
+
+    for _ in range(nit):
         #     // -------------------------------------------------------------
         #     // First step, x update : (I+beta ATA)x = AT (-lambda+beta*ATy)
         #     // -------------------------------------------------------------
-        tmp1=xp.subtract(xp.multiply(y1,beta),lambda1)
-        tmp2=xp.subtract(xp.multiply(y2,beta),lambda2)
-        ftmp1=xp.fft.fft2(tmp1.reshape(n0,n1)).flatten()
-        ftmp2=xp.fft.fft2(tmp2.reshape(n0,n1)).flatten()
-        
-        #Computes w = conj(u) * v
-        ftmp1=xp.multiply(xp.conj(fphi1), ftmp1)
-        ftmp2=xp.multiply(xp.conj(fphi2), ftmp2)
+        tmp1 = xp.subtract(xp.multiply(y1, beta), lambda1)
+        tmp2 = xp.subtract(xp.multiply(y2, beta), lambda2)
+        ftmp1 = xp.fft.fft2(tmp1.reshape(n0, n1)).flatten()
+        ftmp2 = xp.fft.fft2(tmp2.reshape(n0, n1)).flatten()
 
-        #fx = (tmp1 + tmp2) / fphi;
-        fx=xp.divide(xp.add(ftmp1,ftmp2),fphi)
+        # Computes w = conj(u) * v
+        ftmp1 = xp.multiply(xp.conj(fphi1), ftmp1)
+        ftmp2 = xp.multiply(xp.conj(fphi2), ftmp2)
+
+        # fx = (tmp1 + tmp2) / fphi;
+        fx = xp.divide(xp.add(ftmp1, ftmp2), fphi)
 
         #     // --------------------------------------------------------
         #     // Second step y update : y = prox_{f1/beta}(Ax+lambda/beta)
         #     // --------------------------------------------------------
-        
-        ftmp1=xp.multiply(fphi1,fx)
-        ftmp2=xp.multiply(fphi2,fx)
-        tmp1=xp.fft.ifft2(ftmp1.reshape(n0,n1),norm="forward").flatten().real #TODO ifft or ifftn ??
-        tmp2=(xp.fft.ifft2(ftmp2.reshape(n0,n1),norm="forward").flatten()).real
-        tmp1=xp.divide(tmp1,n)  #normalize_array(tmp1)
-        tmp2=xp.divide(tmp2,n) #normalize_array(tmp2)
-        
-        y1,y2=update_y(d1u0, d2u0, tmp1, tmp2, lambda1, lambda2, beta, xp)
+
+        ftmp1 = xp.multiply(fphi1, fx)
+        ftmp2 = xp.multiply(fphi2, fx)
+        tmp1 = xp.fft.ifft2(ftmp1.reshape(n0, n1), norm="forward").flatten().real
+        tmp2 = xp.fft.ifft2(ftmp2.reshape(n0, n1), norm="forward").flatten().real
+        tmp1 = xp.divide(tmp1, n)  # normalize_array(tmp1)
+        tmp2 = xp.divide(tmp2, n)  # normalize_array(tmp2)
+
+        y1, y2 = update_y(d1u0, d2u0, tmp1, tmp2, lambda1, lambda2, beta, xp)
 
         #     // --------------------------
         #     // Third step lambda update
         #     // --------------------------
-        lambda1=lambda1+xp.multiply(beta,xp.subtract(tmp1,y1))
-        lambda2=lambda2+xp.multiply(beta,xp.subtract(tmp2,y2))
+        lambda1 = lambda1 + xp.multiply(beta, xp.subtract(tmp1, y1))
+        lambda2 = lambda2 + xp.multiply(beta, xp.subtract(tmp2, y2))
 
     # // Last but not the least : u = u0 - (psi * x)
-    ftmp1=xp.multiply(fx,fpsi)
-    u=xp.fft.ifft2(ftmp1.reshape(n0,n1),norm="forward").flatten().real #TODO ifft or ifftn ??
-    u=xp.divide(u,n)
-    u=xp.subtract(u0,u)
-    
+    ftmp1 = xp.multiply(fx, fpsi)
+    u = xp.fft.ifft2(ftmp1.reshape(n0, n1), norm="forward").flatten().real
+    u = xp.divide(u, n)
+    u = xp.subtract(u0, u)
+
     return u
 
-def vsnr2d(img, filters, nite=20, beta=10., xp=np):
+
+def vsnr2d(img, filters, nite=20, beta=10.0, xp=np):
     r"""
     Calculate the corrected image using the 2D-VSNR algorithm in libvsnr2d.dll
 
-    .. note:
+    Notes
+    -----
     To ease code comparison with the original onde, most of the variable names
     have been kept as nearly as possible during the code transcription.
     Accordingly, PEP8 formatting compatibility may not be always respected.
@@ -278,25 +319,36 @@ def vsnr2d(img, filters, nite=20, beta=10., xp=np):
     nite: int, optional
         Number of iterations in the denoising processing
     beta: float
-        The regularization parameter in the VSNR model. Controls the trade-off between the data fidelity term and the regularization term. A higher beta value gives more weight to the regularization term, which encourages sparser solutions but may result in more denoising (and potentially more distortion). A lower beta value gives more weight to the data fidelity term, which encourages solutions that are closer to the original image but may result in less denoising.
+        The regularization parameter in the VSNR model. Controls the trade-off between the data
+        fidelity term and the regularization term. A higher beta value gives more weight to the
+        regularization term, which encourages sparser solutions but may result in more denoising
+        (and potentially more distortion). A lower beta value gives more weight to the data fidelity
+        term, which encourages solutions that are closer to the original image but may result in
+        less denoising.
 
     Returns
     -------
     img_corr: numpy.ndarray((n0, n1))
         The corrected image
     """
-    img=xp.asarray(img)
+    img = xp.asarray(img)
+    vmax = vmax_encoding(img)
+    dtype = img.dtype
+
     n0, n1 = img.shape
     u0 = img.flatten()
     u = xp.zeros_like(u0)
 
     # calculation
-    vmax = u0.max()
-    u=VSNR_2D_FIJI_GPU(filters, u0, n0, n1, nite, beta, vmax, xp)
+    max_value = u0.max()
+    u = compute_vsnr(filters, u0, n0, n1, nite, beta, max_value, xp)
 
     # reshaping
     img_corr = xp.array(u).reshape(n0, n1).astype(float)
-    
-    img_corr = xp.clip(img_corr, 0, 1)
+
+    img_corr = xp.clip(img_corr, 0, vmax)
+
+    # cast to original dtype
+    img_corr = img_corr.astype(dtype)
 
     return img_corr

src/pyvsnr/vsnr2d_cuda.py
@@ -1,9 +1,12 @@
+"""
+This python module is a wrapper for the cuda implementation of the VSNR2D
+"""
 import os
 import numpy as np
 import pathlib
 from ctypes import POINTER, c_int, c_float, CDLL
+from .vsnr2d import vmax_encoding
 
-# precompiled path = file path/../precompiled
 PRECOMPILED_PATH = pathlib.Path(__file__).parent / 'precompiled'
 
 def get_dll():
@@ -41,7 +44,8 @@ def vsnr2d_cuda(img, filters, nite=20, beta=10., nblocks='auto'):
     r"""
     Calculate the corrected image using the 2D-VSNR algorithm in libvsnr2d.dll
 
-    .. note:
+    Notes
+    -----
     To ease code comparison with the original onde, most of the variable names
     have been kept as nearly as possible during the code transcription.
     Accordingly, PEP8 formatting compatibility is not always respected.
@@ -72,6 +76,8 @@ def vsnr2d_cuda(img, filters, nite=20, beta=10., nblocks='auto'):
     """
     length = len(filters)
     n0, n1 = img.shape
+    dtype = img.dtype
+    vmax = vmax_encoding(img)
 
     # psis definition from filters
     psis = []
@@ -104,13 +110,16 @@ def vsnr2d_cuda(img, filters, nite=20, beta=10., nblocks='auto'):
         nblocks = max(nblocks_max, nblocks)
 
     # calculation
-    vmax = u0.max()
+    max_value = u0.max()
     vsnr_func = get_vsnr2d()
-    vsnr_func(psis_, length, u0_, n0, n1, nite, beta, u_, nblocks, vmax)
+    vsnr_func(psis_, length, u0_, n0, n1, nite, beta, u_, nblocks, max_value)
 
     # reshaping
     img_corr = np.array(u_).reshape(n0, n1).astype(float)
 
-    img_corr = np.clip(img_corr, 0, 1)
+    img_corr = np.clip(img_corr, 0, vmax)
+
+    # recast to original dtype
+    img_corr = img_corr.astype(dtype)
     
     return img_corr

tests/test_cuda_equals_py.py
@@ -1,3 +1,7 @@
+"""
+Pytest file to verify that the python code is equivalent to the cuda code
+"""
+
 import cupy as cp
 import numpy as np
 import skimage
@@ -8,6 +12,8 @@ DIRNAME = pathlib.Path(__file__).parent
 PRECOMPILED_PATH = DIRNAME.parent / 'src' / 'pyvsnr' / 'precompiled'
 
 from src.pyvsnr.vsnr2d import *
+from tests.utils import stripes_addition
+
 xp=cp
 
 cuda_code = """
@@ -343,7 +349,8 @@ def vsnr2d_cuda(img, filters, nite=20, beta=10., nblocks='auto'):
     r"""
     Calculate the corrected image using the 2D-VSNR algorithm in libvsnr2d.dll
 
-    .. note:
+    Notes
+    -----
     To ease code comparison with the original onde, most of the variable names
     have been kept as nearly as possible during the code transcription.
     Accordingly, PEP8 formatting compatibility is not always respected.
@@ -659,18 +666,17 @@ def test_vsnr_admm():
 
 
 def test_cuda_equals_cupy_numpy():
-    img = skimage.data.camera().astype(np.float32)/255
+    """
+    Test if the cuda code is equivalent to the cupy code
+    """
+    img = skimage.data.camera()
     
     maxit=20
     filters = [{'name':'Dirac', 'noise_level':0.4}]
 
-   # Add vertical stripes
-    for i in range(img.shape[0]):
-        img[i] += 0.2 * (np.random.random() - 0.5)
+    img = stripes_addition(img, 0.2)
 
-    img=np.clip(img,0,1)
-
-    img_corr_cupy = vsnr2d(xp.asarray(img), filters, nite=maxit, beta=10., xp=xp)
+    img_corr_py = vsnr2d(xp.asarray(img), filters, nite=maxit, beta=10., xp=xp)
     img_corr_cuda = vsnr2d_cuda(img, filters, nite=maxit, beta=10., nblocks='auto')
     
-    xp.testing.assert_allclose(img_corr_cuda,img_corr_cupy,atol=1e-3)
+    xp.testing.assert_allclose(img_corr_cuda,img_corr_py,atol=1e-3)

tests/utils.py
@@ -1,54 +1,91 @@
+"""
+Utilities for tests
+"""
 import os
 import time
 import matplotlib.pyplot as plt
 import numpy as np
 import cupy as cp
 
-from skimage._shared.utils import _supported_float_type, check_shape_equality, warn
+from skimage._shared.utils import (
+    _supported_float_type,
+    check_shape_equality,
+    warn,
+)
 from skimage.util.dtype import dtype_range
-from pyvsnr.vsnr2d import create_gabor
-from pyvsnr import vsnr2d, vsnr2d_cuda
+from src.pyvsnr.vsnr2d import create_gabor, vmax_encoding
+from src.pyvsnr import vsnr2d, vsnr2d_cuda
 
-def measure_vsnr_cuda(img, filters, nite=20, beta=10.):
+
+def measure_vsnr(
+    algo, img, filters, nite=20, beta=10.0, nblocks="auto", xp=np
+):
+    """
+    Measure the execution time of a vsnr2d algorithm
+    """
     t0 = time.perf_counter()
-    img_corr_cuda = vsnr2d_cuda(img, filters, nite=nite, beta=beta, nblocks='auto')
-    process_time = round(time.perf_counter() - t0,3)
-    print("\033[92m" + f"CUDA: {process_time} sec" + "\033[0m")
 
+    if algo == vsnr2d_cuda:
+        img_corr = algo(img, filters, nite=nite, beta=beta, nblocks=nblocks)
+    else:
+        img_corr = algo(img, filters, nite=nite, beta=beta, xp=xp)
+
+    process_time = round(time.perf_counter() - t0, 3)
+
+    if algo == vsnr2d_cuda:
+        print("\033[92m" + f"CUDA: {process_time} sec" + "\033[0m")
+    else:
+        print("\033[94m" + f"{xp.__name__}: {process_time} sec" + "\033[0m")
+
+    return img_corr
+
+
+def measure_vsnr_cuda(img, filters, nite=20, beta=10.0, nblocks="auto"):
+    """
+    Measure the execution time of the vsnr2d_cuda algorithm
+    """
+    img_corr_cuda = measure_vsnr(
+        vsnr2d_cuda, img, filters, nite=nite, beta=beta, nblocks=nblocks
+    )
     return img_corr_cuda
 
-def measure_vsnr_py(img, filters, nite=20, beta=10., xp=np):
-    t0 = time.perf_counter()
-    img_corr_py = vsnr2d(img, filters, nite=nite, beta=beta, xp=xp)
-    process_time = round(time.perf_counter() - t0,3)
-    print("\033[94m" + f"{xp.__name__}: {process_time} sec" + "\033[0m")
 
-    return img_corr_py
-
-def measure_vsnr_cupy(img, filters, nite=20, beta=10.):
-    img_corr_cupy = measure_vsnr_py(img, filters, nite=nite, beta=beta, xp=cp)
+def measure_vsnr_cupy(img, filters, nite=20, beta=10.0):
+    """
+    Measure the execution time of the vsnr2d_cupy algorithm
+    """
+    img_corr_cupy = measure_vsnr(
+        vsnr2d, img, filters, nite=nite, beta=beta, xp=cp
+    )
     return img_corr_cupy
 
-def measure_vsnr_numpy(img, filters, nite=20, beta=10.):
-    img_corr_numpy = measure_vsnr_py(img, filters, nite=nite, beta=beta, xp=np)
+
+def measure_vsnr_numpy(img, filters, nite=20, beta=10.0):
+    """
+    Measure the execution time of the vsnr2d_numpy algorithm
+    """
+    img_corr_numpy = measure_vsnr(
+        vsnr2d, img, filters, nite=nite, beta=beta, xp=np
+    )
     return img_corr_numpy
 
-def print_max_diff(img_corr_py, img_corr_cuda, xp):
-    difference = xp.abs(xp.asarray(img_corr_cuda) - xp.asarray(img_corr_py)).max()
-    print(f"Greatest difference between CUDA and {xp.__name__}: {format(difference)}")
-    assert xp.allclose(img_corr_cuda, img_corr_py, atol=1e-4), "CUDA and Python implementations are not equal"
 
-def vmax_encoding(arr):
-    """ Basic function to estimate vmax value related to array type encoding"""
-    if arr.max() <= 1.:
-        return 1.
-    elif arr.max() <= 255:
-        return 255
-    elif arr.max() <= 65535:
-        return 65535
-    else:
-        raise IOError("maximum value over 65535 !!!")
-    
+def print_max_diff(img_corr_py, img_corr_cuda, xp):
+    """
+    Print the maximum difference between the CUDA and Python implementations
+    """
+    difference = xp.abs(
+        xp.asarray(img_corr_cuda) - xp.asarray(img_corr_py)
+    ).max()
+    print(
+        f"Greatest difference between CUDA and {xp.__name__}:"
+        f" {format(difference)}"
+    )
+    assert xp.allclose(
+        img_corr_cuda, img_corr_py, atol=1e-2 # usually 1e-4 but if imùage is [0;255] range it is 1e-2
+    ), "CUDA and Python implementations are not equal"
+
+
 def pad_centered(arr, shape_ref, value=0):
     """
     Return a centered ND-array with a surrounding padding 'value'
@@ -66,14 +103,8 @@ def pad_centered(arr, shape_ref, value=0):
     -------
     arr_pad: numpy.ndarray()
         The resulting padded array
-
-
-    .. plot::
-
-        from common_ex.core.ex_array import ex_pad_centered
-        ex_pad_centered(make_plots=True)
     """
-    assert (len(shape_ref) == len(arr.shape))
+    assert len(shape_ref) == len(arr.shape)
 
     dim = len(shape_ref)
     arr_pad = arr.copy()
@@ -93,12 +124,13 @@ def pad_centered(arr, shape_ref, value=0):
             arr_pad = np.pad(arr_pad, width, constant_values=value)
         # cutting
         else:
-            arr_pad = arr_pad[-gap2:-gap2 + shape_ref[k], ...]
+            arr_pad = arr_pad[-gap2 : -gap2 + shape_ref[k], ...]
 
         # return to original axis
         arr_pad = np.swapaxes(arr_pad, 0, k)
 
     return arr_pad
+
 
 def mean_squared_error(image0, image1):
     """
@@ -113,17 +145,11 @@ def mean_squared_error(image0, image1):
     -------
     mse : float
         The mean-squared error (MSE) metric.
-
-    Notes
-    -----
-    .. versionchanged:: 0.16
-        This function was renamed from ``skimage.measure.compare_mse`` to
-        ``skimage.metrics.mean_squared_error``.
-
     """
     check_shape_equality(image0, image1)
     image0, image1 = _as_floats(image0, image1)
     return np.mean((image0 - image1) ** 2, dtype=np.float64)
+
 
 def _as_floats(image0, image1):
     """
@@ -133,6 +159,7 @@ def _as_floats(image0, image1):
     image0 = np.asarray(image0, dtype=float_type)
     image1 = np.asarray(image1, dtype=float_type)
     return image0, image1
+
 
 def peak_signal_noise_ratio(image_true, image_test, *, data_range=None):
     """
@@ -154,32 +181,29 @@ def peak_signal_noise_ratio(image_true, image_test, *, data_range=None):
     psnr : float
         The PSNR metric.
 
-    Notes
-    -----
-    .. versionchanged:: 0.16
-        This function was renamed from ``skimage.measure.compare_psnr`` to
-        ``skimage.metrics.peak_signal_noise_ratio``.
-
     References
     ----------
-    .. [1] https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
+    [1] https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
 
     """
-    if type(image_test) == cp.ndarray:
+    if isinstance(image_test, cp.ndarray):
         image_test = cp.asnumpy(image_test)
-        
+
     check_shape_equality(image_true, image_test)
 
     if data_range is None:
         if image_true.dtype != image_test.dtype:
-            warn("Inputs have mismatched dtype.  Setting data_range based on "
-                 "image_true.")
+            warn(
+                "Inputs have mismatched dtype.  Setting data_range based on "
+                "image_true."
+            )
         dmin, dmax = dtype_range[image_true.dtype.type]
         true_min, true_max = np.min(image_true), np.max(image_true)
         if true_max > dmax or true_min < dmin:
             raise ValueError(
                 "image_true has intensity values outside the range expected "
-                "for its data type. Please manually specify the data_range.")
+                "for its data type. Please manually specify the data_range."
+            )
         if true_min >= 0:
             # most common case (255 for uint8, 1 for float)
             data_range = dmax
@@ -189,57 +213,86 @@ def peak_signal_noise_ratio(image_true, image_test, *, data_range=None):
     image_true, image_test = _as_floats(image_true, image_test)
 
     err = mean_squared_error(image_true, image_test)
-    return 10 * np.log10((data_range ** 2) / err)
+
+    if err == 0:
+        return np.inf
+    
+    return 10 * np.log10((data_range**2) / err)
+
 
 def print_psnr(img, noisy_img, img_corr_py, img_corr_cuda):
-    if type(img_corr_py) == cp.ndarray:
+    """
+    Print the PSNR of the noisy image, the CUDA corrected image and the Python
+    """
+    if isinstance(img_corr_py, cp.ndarray):
         img_corr_py = img_corr_py.get()
 
     psnr_noisy = peak_signal_noise_ratio(img, noisy_img)
     psnr_corrected_cuda = peak_signal_noise_ratio(img, img_corr_cuda)
     psnr_corrected_py = peak_signal_noise_ratio(img, img_corr_py)
 
-    print("\033[95m" + f"PSNR noisy image: {np.round(psnr_noisy, 2)}dB" + "\033[0m")
-    print("\033[92m" + f"PSNR CUDA corrected image: {np.round(psnr_corrected_cuda, 2)}dB" + "\033[0m")
-    print("\033[94m" + f"PSNR Python corrected image: {np.round(psnr_corrected_py, 2)}dB" + "\033[0m")
+    print(
+        "\033[95m"
+        + f"PSNR noisy image: {np.round(psnr_noisy, 2)}dB"
+        + "\033[0m"
+    )
+    print(
+        "\033[92m"
+        + f"PSNR CUDA corrected image: {np.round(psnr_corrected_cuda, 2)}dB"
+        + "\033[0m"
+    )
+    print(
+        "\033[94m"
+        + f"PSNR Python corrected image: {np.round(psnr_corrected_py, 2)}dB"
+        + "\033[0m"
+    )
 
-def plot_results(img, noisy_img, img_corr_py, img_corr_cuda, xp, save_plots=False, title="fig.png"):
-    if type(img_corr_py) == cp.ndarray:
+
+def plot_results(img, noisy_img, img_corr_py, img_corr_cuda, xp, save_plots=False, title="fig.png", vmax=1):
+    """
+    Plot the original image, the noisy image, the Python corrected image and
+    """
+    if isinstance(img_corr_py, cp.ndarray):
         img_corr_py = cp.asnumpy(img_corr_py)
 
     plt.figure(figsize=(15, 5))
     plt.subplot(1, 4, 1)
-    plt.imshow(img, vmin=0, vmax=1)
-    plt.title('Original image')
-    plt.axis('off')
+    plt.imshow(img, vmin=0, vmax=vmax)
+    plt.title("Original image")
+    plt.axis("off")
 
     plt.subplot(1, 4, 2)
-    plt.imshow(noisy_img, vmin=0, vmax=1)
-    plt.title('Noisy image')
-    plt.axis('off')
+    plt.imshow(noisy_img, vmin=0, vmax=vmax)
+    plt.title("Noisy image")
+    plt.axis("off")
 
     plt.subplot(1, 4, 3)
-    plt.imshow(img_corr_py, vmin=0, vmax=1)
-    plt.title(f'Cleaned {xp.__name__} image')
-    plt.axis('off')
+    plt.imshow(img_corr_py, vmin=0, vmax=vmax)
+    plt.title(f"Cleaned {xp.__name__} image")
+    plt.axis("off")
 
     plt.subplot(1, 4, 4)
-    plt.imshow(img_corr_cuda, vmin=0, vmax=1)
-    plt.title('Cleaned CUDA image')
-    plt.axis('off')
+    plt.imshow(img_corr_cuda, vmin=0, vmax=vmax)
+    plt.title("Cleaned CUDA image")
+    plt.axis("off")
 
     if save_plots:
-        plt.savefig(os.path.join(os.path.dirname(__file__), 'images', title), bbox_inches='tight')
+        plt.savefig(
+            os.path.join(os.path.dirname(__file__), "images", title),
+            bbox_inches="tight",
+        )
 
     plt.show()
 
-def stripes_addition(img_base, amplitude):
-    np.random.seed(0) # DEBUG
-    # norm_img = np.linalg.norm(img_base) # DEBUG
-    # test=np.zeros_like(img_base) # DEBUG
+
+def stripes_addition(img_base, amplitude, seed=None):
+    """
+    Add stripes defects in a image
+    """
+    np.random.seed(seed)
 
     noisy_img = img_base.copy()
-    vmax=vmax_encoding(noisy_img)
+    vmax = vmax_encoding(noisy_img)
 
     noisy_img = noisy_img / vmax
 
@@ -247,17 +300,12 @@ def stripes_addition(img_base, amplitude):
         noise = amplitude * (np.random.random() - 0.5)
         noisy_img[i] += noise
 
-        # test[i] = noise # DEBUG
-
-    # print("norme de l'image : ", norm_img) # DEBUG
-    # print("norme du bruit ajouté : ", np.linalg.norm(test)) # DEBUG
-    # print(f"noise norm : {round(np.linalg.norm(test) / norm_img * 100, 3)} %") # DEBUG
-
     noisy_img = np.clip(noisy_img, 0, 1)
 
     return noisy_img
 
-def curtains_addition(img_ref, seed=0, amplitude=0.2, sigma=(3,40), angle=0, threshold=0.999):
+
+def curtains_addition(img_ref, seed=None, amplitude=0.2, sigma=(3, 40), angle=0, threshold=0.999):
     """
     Add curtains effects in a image
 
@@ -282,11 +330,11 @@ def curtains_addition(img_ref, seed=0, amplitude=0.2, sigma=(3,40), angle=0, thr
     img: numpy.ndarray((m, n))
         Image with curtains
     """
-    assert (amplitude >= 0.)
+    assert amplitude >= 0.0
 
     np.random.seed(seed)
 
-    n0,n1 = img_ref.shape
+    n0, n1 = img_ref.shape
     sigmax, sigmay = sigma
 
     # relative to absolute noise amplitude conversion
@@ -294,7 +342,9 @@ def curtains_addition(img_ref, seed=0, amplitude=0.2, sigma=(3,40), angle=0, thr
     amplitude *= vmax
 
     # curtains definition (from gabor filter) and location
-    psi = create_gabor(n0, n1, 0.2, sigmax, sigmay, angle=angle, phase=0, lambda_=0., xp=np).reshape(n0, n1)
+    psi = create_gabor(
+        n0, n1, 0.2, sigmax, sigmay, angle=angle, phase=0, lambda_=0.0, xp=np
+    ).reshape(n0, n1)
 
     psi *= 0.01 / psi.max()  # renormalization
     psi = pad_centered(psi, img_ref.shape, value=0)